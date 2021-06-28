19c320fe6220a02fa3f4ce2a648c6399744b7261
AlchinVS <79061188133@ya.ru>
2021-06-29 01:28:06 +0300

    Все таблицы создаются.

--- dss_core.sql
+++ dss_core.sql
@@ -1,14 +1,15 @@
 
-
-create table core.hub_suppliers (
+DROP TABLE IF EXISTS core.hub_suppliers ;
+create table core.hub_suppliers(
     hub_sup_key SERIAL PRIMARY KEY,
     sat_sup_id int,
-    source_system string,
-    processed_dttm datetime
-  --  UNIQUE(sat_sup_id)
+    source_system varchar(20),
+    processed_dttm timestamp
 );
 
 
+
+DROP TABLE IF EXISTS core.sat_suppliers ;
 create table core.sat_suppliers (
     sat_sup_id int4,
     SuppliersName varchar(25),
@@ -18,15 +19,19 @@ create table core.sat_suppliers (
     Descr varchar(101)
 );
 
+
+
+DROP TABLE IF EXISTS core.hub_products ;
 create table core.hub_products (
     hub_prod_key SERIAL PRIMARY KEY,
     sat_prod_id int,
-    source_system string,
-    processed_dttm datetime
- --   UNIQUE(sat_sup_id)
+    source_system varchar(20),
+    processed_dttm timestamp
+
 );
 
 
+DROP TABLE IF EXISTS core.sat_products  ;
 create table core.sat_products (
     sat_prod_id int4,
     ProductName varchar(55),
@@ -35,13 +40,13 @@ create table core.sat_products (
     RetailPrice numeric(15,2)
 );
 
+
 drop table if exists core.hub_orders;
 create table core.h_orders (
 hub_orders_key SERIAL PRIMARY KEY,
 order_id int,
 source_system varchar(20),
-processed_dttm timestamp,
---UNIQUE(order_id)
+processed_dttm timestamp
 );
 
 drop table if exists core.s_orders;
@@ -59,15 +64,15 @@ l_prod_supl_pk SERIAL PRIMARY KEY,
 hub_sup_key int,
 hub_prod_key int,
 source_system varchar(20),
-processed_dttm timestamp,
+processed_dttm timestamp
 );
 
 
 drop table if exists core.l_prod_ord;
-create table core.l_prod_supl (
+create table core.l_prod_ord (
 l_prod_ord_pk SERIAL PRIMARY KEY,
 hub_sup_key int,
 hub_orders_key int,
 source_system varchar(20),
-processed_dttm timestamp,
+processed_dttm timestamp
 );
